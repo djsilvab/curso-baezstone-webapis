4e97ae20a79668cfd831912ac2ef56ca460a1c30
David Silva Bazan <djsilvab@gmail.com>
2025-05-07 17:53:38 -0500

    tipos de estados

---
+++ BaezStone.MagicVilla.Api/Store/VillaStore.cs
@@ -0,0 +1,19 @@
+﻿using BaezStone.MagicVilla.Api.Models.Dto;
+
+namespace BaezStone.MagicVilla.Api.Store;
+public static class VillaStore
+{
+    public static List<VillaDto> villaList = new List<VillaDto>
+    {
+        new VillaDto
+        {
+            Id = 1,
+            Nombre = "Villa 1"            
+        },
+        new VillaDto
+        {
+            Id = 2,
+            Nombre = "Villa 2"            
+        }
+    };
+}
