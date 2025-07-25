4150002a127cf5edd2581927df9526f08c777a8e
David Silva Bazan <djsilvab@gmail.com>
2025-07-24 22:05:29 -0500

    Ejemplo de HttpDelete

--- BaezStone.MagicVilla.Api/Controllers/VillaController.cs
+++ BaezStone.MagicVilla.Api/Controllers/VillaController.cs
@@ -34,19 +34,33 @@ public class VillaController : ControllerBase
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public ActionResult<VillaDto> CreateVilla([FromBody] VillaDto villaDto)
     {
-        if (!ModelState.IsValid) return BadRequest(ModelState);
         if (villaDto is null) return BadRequest("El objeto es nulo.");
+        if (!ModelState.IsValid) return BadRequest(ModelState);        
         if(villaDto.Id > 0) return StatusCode(StatusCodes.Status400BadRequest, "El Id debe ser cero");
-        if(VillaStore.villaList.FirstOrDefault(x => x.Nombre.ToLower() == villaDto.Nombre.ToLower()) is not null) 
+        if(VillaStore.villaList.FirstOrDefault(x => x.Nombre.Equals(villaDto.Nombre, StringComparison.OrdinalIgnoreCase)) is not null)
         { 
             ModelState.AddModelError("NombreExistente", "Ya existe una villa con ese nombre.");
             return BadRequest(ModelState);
         }
 
-        villaDto.Id = VillaStore.villaList.OrderByDescending(v => v.Id).FirstOrDefault()?.Id + 1 ?? 1;
+        villaDto.Id = (VillaStore.villaList.OrderByDescending(v => v.Id).FirstOrDefault()?.Id ?? 0) + 1;
 
         //if (VillaStore.villaList.Any(v => v.Id == villaDto.Id)) return BadRequest("Ya existe una villa con ese Id.");
         VillaStore.villaList.Add(villaDto);
         return CreatedAtAction(nameof(GetVilla), new { id = villaDto.Id }, villaDto);
+    }
+
+    [HttpDelete("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult DeleteVilla(int id)
+    {
+        //Se utiliza IActionResult porque no se retorna un objeto
+        if (id == 0) return BadRequest("Id debe ser mayor a cero.");
+        var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+        if (villa is null) return NotFound();
+        VillaStore.villaList.Remove(villa);
+        return NoContent(); //204 No Content
     }
 }
