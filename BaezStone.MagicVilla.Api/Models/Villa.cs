f02d73590b53194b870973d1604cca6ee58c6a76
David Silva Bazan <djsilvab@gmail.com>
2026-04-05 23:56:14 -0500

    Integración inicial de EF Core y ampliación de modelos
    Se añaden dependencias de Entity Framework Core para SQL Server y herramientas asociadas. Se actualizan las clases Villa y VillaDto con nuevas propiedades (Detalle, Tarifa, ImagenUrl, Amenidad, etc.), cambios de tipos de datos y anotaciones de validación para preparar la integración con base de datos y mejorar la estructura de los modelos.

--- BaezStone.MagicVilla.Api/Models/Villa.cs
+++ BaezStone.MagicVilla.Api/Models/Villa.cs
@@ -1,7 +1,18 @@
-﻿namespace BaezStone.MagicVilla.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BaezStone.MagicVilla.Api.Models;
 public class Villa
 {
+    [Key]
     public int Id { get; set; }
     public string Nombre { get; set; }
+    public string Detalle { get; set; }
+    [Required]
+    public double Tarifa { get; set; }
+    public int Ocupantes { get; set; }
+    public int MetrosCuadrados { get; set; }
+    public string ImagenURL { get; set; }
+    public string Amenidad { get; set; }
     public DateTime FechaCreacion { get; set; }
+    public DateTime FechaActualizacion { get; set; }
 }
